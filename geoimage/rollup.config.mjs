529dcb239b3fccacde8f6d7b71ccda16c566dbc4
Vojtěch Dubrovský <v.dubr@hotmail.com>
2023-06-14 13:02:19 +0200

    Setup actions

--- geoimage/rollup.config.mjs
+++ geoimage/rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+// import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
